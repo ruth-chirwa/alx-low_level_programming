db74efdb020bf07d77c91bf86814e8aeeb5c90b1
ruth-chirwa <chirwaruth647@gmail.com>
2023-04-20 06:38:36 -0700

    How to create an executable file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts - This is a function that writes a line of output to the screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
