8339b356fd4481533c6fe03e5f828468286a2d37
ruth-chirwa <chirwaruth647@gmail.com>
2023-04-21 04:23:50 -0700

    Learning all about the c variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this contains if else statements
+ * they randomly generate conditions
+ * based on random number generation
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
