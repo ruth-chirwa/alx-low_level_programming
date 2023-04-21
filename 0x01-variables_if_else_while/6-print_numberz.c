0b00d46d54f824d03cb27a0fe96d182b57546d3d
ruth-chirwa <chirwaruth647@gmail.com>
2023-04-21 07:06:58 -0700

    Commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
